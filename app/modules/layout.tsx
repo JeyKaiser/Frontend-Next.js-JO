a65e5d78c9cea7d90dbb62a3c00401fd57533558
Jefferson Chacon <jchacon@jo.local>
2025-11-15 15:43:46 -0500

    Estudio Detallado de Manage.py

--- app/modules/layout.tsx
+++ app/modules/layout.tsx
@@ -1,25 +1,38 @@
+'use client';
+
+import { useAuth } from '@/app/modules/(auth)/context/AuthContext'; 
 import Navbar from '@/app/globals/components/organisms/Navbar';
 import Sidebar from '@/app/globals/components/organisms/sidebar';
 import { SAPDataProvider } from '@/app/contexts/SAPDataContext';
+import { use } from 'react';
 
 export default function ModulesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    // Envolvemos todo con el SAPDataProvider para que esté disponible en todos los módulos
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>; // O un componente de carga más elaborado
+  }
+  
+   return (
     <SAPDataProvider>
       <div className="min-h-screen bg-secondary-50">
-        {/* Renderiza la barra de navegación superior */}
-        <Navbar />
-        {/* Renderiza el menú lateral */}
-        <Sidebar />
-
-        {/* Define el área principal para el contenido de la página */}
-        <main className="pl-64 pt-16">
-          {children} {/* Aquí Next.js insertará el contenido de tu page.tsx */}
-        </main>
+        {isAuthenticated ? (
+          <>
+            <Navbar />
+            <Sidebar />
+            <main className="pl-64 pt-16">
+              {children}
+            </main>
+          </>
+        ) : (
+          <main className="min-h-screen">
+            {children}
+          </main>
+        )}
       </div>
     </SAPDataProvider>
   );
